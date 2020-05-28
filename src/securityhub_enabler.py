1b4ab6c82f57b4c8b5534f384a8628685dad8f8c
Jason Cornick <cornickj@amazon.com>
2020-05-28 16:45:31 -0500

    Fix arn for disabling AWS Standards

--- src/securityhub_enabler.py
+++ src/securityhub_enabler.py
@@ -31,6 +31,7 @@ https://github.com/awslabs/aws-securityhub-multiaccount-scripts
 import boto3
 import json
 import requests
+import urllib3
 import os
 import logging
 from botocore.exceptions import ClientError
@@ -72,10 +73,10 @@ def send(
         'content-type': '',
         'content-length': str(len(json_responseBody))
     }
-
+    http = urllib3.PoolManager()
     try:
-        response = requests.put(responseUrl,
-                                data=json_responseBody,
+        response = http.request('PUT',responseUrl,
+                                body=json_responseBody,
                                 headers=headers)
         print("Status code: " + response.reason)
     except Exception as e:
@@ -271,7 +272,7 @@ def process_security_standards(sh_client, partition, region, account):
                         f"{account} in {region}")
         else:
             sh_client.batch_disable_standards(
-                StandardsSubscriptionArns=[AWS])
+                StandardsSubscriptionArns=[AWS_STANDARD_ARN])
             LOGGER.info(f"Disabled AWS Foundational Security Best Practices "
                         f"v1.0.0 Security Standard in Account {account} in "
                         f"{region}")
